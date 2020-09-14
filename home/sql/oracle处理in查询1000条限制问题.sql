596e7c1d22d05d6a6375b47d2d35a5f3f3942218
汪正朋 <wangzhengpeng@ruiqin.net>
2020-09-14 22:17:09 +0800

    删除图片

--- home/sql/oracle处理in查询1000条限制问题.sql
+++
@@ -1,2 +0,0 @@
--- 改成多个or查询的语句
-select xxx from xxx where xxx in (xx,xxx...) or xxx in (xx,xx,...) or xxx in (xx,xx,...);
--- home/sql/time.sql
+++
@@ -1,8 +0,0 @@
-
--- 时间保留毫秒
--- DATETIME(3) (字段长度)
-ALTER TABLE `rfq_quotation`  ADD  DAC_QUOTATION_DATE DATETIME(3) NULL COMMENT '报价精确时间' ;
-
-UPDATE `rfq_quotation` rq SET rq.`DAC_QUOTATION_DATE` = '2017-05-09 10:48:17.196' WHERE `REQUEST_ID` = '34fee4d6-fdcd-4492-bf56-0563c13bf4cd' ;
--- 查询含有毫秒值的时间
-SELECT NOW(3);
